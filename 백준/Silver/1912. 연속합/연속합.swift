1c118041d427b5f77969bb1bc0664ce7c6d51b9e
Picnic <59194210+picnicodesm@users.noreply.github.com>
2025-05-01 08:37:09 +0900

    [Silver II] Title: 연속합, Time: 36 ms, Memory: 76112 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.swift
@@ -0,0 +1,10 @@
+let n = Int(String(readLine()!))!
+let arr = readLine()!.split(separator: " ").map{Int(String($0))!}
+var dp = Array(repeating: -1001, count: 100001)
+
+dp[0] = arr[0]
+
+for i in 1..<n{
+    dp[i] = max(arr[i],dp[i - 1] + arr[i])
+}
+print(dp.max()!)
