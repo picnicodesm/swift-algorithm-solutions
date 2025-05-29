b8ec09fff53388d9b5a002d1b9a2becd9a520cfd
Picnic <59194210+picnicodesm@users.noreply.github.com>
2025-05-29 13:47:11 +0900

    [level 2] Title: 롤케이크 자르기, Time: 912.04 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.swift
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.swift
@@ -6,9 +6,11 @@ func solution(_ topping:[Int]) -> Int {
     var right: [Int: Int] = [:]
     var result = 0
     
-    for i in topping { right[i, default: 0] += 1 }
+    left[topping[0]] = 1
+    for i in 1..<topping.count { right[topping[i], default: 0] += 1 }
+    if left.count == right.count { result += 1 }
     
-    for i in 0..<topping.count-1 {
+    for i in 1..<topping.count-1 {
         left[topping[i], default: 0] += 1
         right[topping[i]] = right[topping[i]]! - 1 == 0 ? nil : right[topping[i]]! - 1
         
