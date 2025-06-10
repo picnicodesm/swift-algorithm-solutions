50a547860f587186f5c1915e600201f7a8a16237
Picnic <59194210+picnicodesm@users.noreply.github.com>
2025-06-10 22:55:46 +0900

    [level 0] Title: 숫자 찾기, Time: 0.04 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+func solution(_ num:Int, _ k:Int) -> Int {
+    if let index = String(num).map { String($0) }.firstIndex(of: String(k)) {
+        return index + 1
+    } else { return -1 }
+}
