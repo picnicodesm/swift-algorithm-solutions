c7d110702a138d935582fa6add0e0ab3eecf2ab4
picnic <59194210+picnicodesm@users.noreply.github.com>
2025-07-17 11:50:25 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 302.94 ms, Memory: 60.5 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.swift
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.swift
@@ -1,15 +1,6 @@
 import Foundation
 
-func solution(_ sequence:[Int], _ k:Int) -> [Int] {
-    // 투 포인터를 이용해야할 것 같아요.
-    // 합이 k가 되는 것을 먼저 찾아야 할 것 같아요.
-    // 그러면 어떻게 합이 k인걸 찾을 수 있을까?
-    // 오름차순이기 때문에 무조건 뒤의 숫자가 크다.
-    // 뒤 포인터를 늘려 가면서 값을 비교한다.
-    // 합이 k를 넘어가면 앞 포인터를 당긴다.
-    // 앞 포인터가 뒷 포인터를 넘어가면 찾을 수 없는 k이다.
-    // 길이도 비교해야 한다.
-    
+func solution(_ sequence:[Int], _ k:Int) -> [Int] {    
     var p1 = 0
     var p2 = 0
     var result: [Int] = [0, sequence.count-1]
