39cef29974f98762e6691ab19bba10c387afb52a
picnic <59194210+picnicodesm@users.noreply.github.com>
2025-08-26 10:10:32 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.15 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.swift
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.swift
@@ -1,71 +1,68 @@
 import Foundation
 
-typealias Pos = (row: Int, col: Int)
+typealias Pos = (r: Int, c: Int)
 
 func solution(_ places:[[String]]) -> [Int] {
-    var result: [Int] = []
-    
-    for room in places {
-        let room: [[String]] = room.map { $0.map { String($0) } }
-        let isSatisfy = check(room)
-        result.append(isSatisfy)
+    return places.map { place in
+        let room = place.map { Array($0) }
+        return checkRoom(room)
     }
-    
-    return result
 }
 
-func check(_ room: [[String]]) -> Int {
-
-    var visited = Array(repeating: Array(repeating: false, count: room.first!.count), count: room.count)
-    let directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, 1), (1, -1), (-1, -1), (2, 0), (-2, 0), (0, 2), (0, -2)]
-    
-    func lookAround(at pos: Pos) -> Bool {
-        
-        for direction in directions {
-            let lookPosition: Pos = (pos.row + direction.0, pos.col + direction.1)
-            
-            if isInTheRoom(lookPosition) && 
-            room[lookPosition.0][lookPosition.1] == "P" && 
-            !visited[lookPosition.0][lookPosition.1] {
-                if getDistance(pos, lookPosition) == 1 { 
-                    return false
-                } else { // 거리가 2인 경우
-                    if pos.row == lookPosition.row { 
-                        let middle = (pos.col + lookPosition.col) / 2
-                        if room[pos.row][middle] != "X" { return false}
-                    } else if pos.col == lookPosition.col {
-                        let middle = (pos.row + lookPosition.row) / 2
-                        if room[middle][pos.col] != "X" { return false}
-                    } else {
-                        if !(room[pos.row][lookPosition.col] == "X" && room[lookPosition.row][pos.col] == "X") { return false }
-                    }
-                }
-                
-            }
-        }
-        
-        return true
-    }
-    
-    
-    for row in 0..<room.count {
-        for col in 0..<room.first!.count {
-            if room[row][col] == "P" {
-                visited[row][col] = true
-                if !lookAround(at: (row, col)) {
+func checkRoom(_ room: [[Character]]) -> Int {
+    for r in 0..<5 {
+        for c in 0..<5 {
+            if room[r][c] == "P" {
+                if !bfs(room, (r, c)) {
                     return 0
                 }
             }
         }
     }
-    
     return 1
 }
 
-func isInTheRoom(_ pos: Pos) -> Bool {
-    return (0..<5).contains(pos.0) && (0..<5).contains(pos.1)
-}
-
-func getDistance(_ p1: Pos, _ p2: Pos) -> Int {
-    return abs(p1.row - p2.row) + abs(p1.col - p2.col)
+func bfs(_ room: [[Character]], _ startPos: Pos) -> Bool {
+    let dx = [0, 0, 1, -1]
+    let dy = [1, -1, 0, 0]
+    
+    var queue: [(pos: Pos, dist: Int)] = [(startPos, 0)]
+    var visited = Array(repeating: Array(repeating: false, count: 5), count: 5)
+    visited[startPos.r][startPos.c] = true
+    
+    var head = 0
+    while head < queue.count {
+        let (current, dist) = queue[head]
+        head += 1
+        
+        if dist == 2 {
+            continue
+        }
+        
+        for i in 0..<4 {
+            let nextR = current.r + dx[i]
+            let nextC = current.c + dy[i]
+            
+            if nextR < 0 || nextR >= 5 || nextC < 0 || nextC >= 5 {
+                continue
+            }
+            
+            if visited[nextR][nextC] {
+                continue
+            }
+            
+            if room[nextR][nextC] == "X" {
+                continue
+            }
+            
+            if room[nextR][nextC] == "P" {
+                return false
+            }
+            
+            visited[nextR][nextC] = true
+            queue.append(((nextR, nextC), dist + 1))
+        }
+    }
+    
+    return true
 }
