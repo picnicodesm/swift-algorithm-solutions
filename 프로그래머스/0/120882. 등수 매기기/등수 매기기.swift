a9fa8c2a0329067b9a3eb936b4dfc08ef3167ca9
picnic <59194210+picnicodesm@users.noreply.github.com>
2025-10-07 21:49:59 +0900

    [level 0] Title: 등수 매기기, Time: 0.15 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.swift
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.swift
@@ -1,9 +1,26 @@
 import Foundation
 
 func solution(_ score:[[Int]]) -> [Int] {
-    let totalScores = score.map { $0[0] + $0[1] }
+    var result: [Int] = Array(repeating: 0, count: score.count)
     
-    let sortedScores = totalScores.sorted(by: >)
+    var ordered = score.sorted(by: { (Double($0[0]) + Double($0[1])) / 2 > (Double($1[0]) + Double($1[1])) / 2})
+    var rank = 1
+    var avg: Double = -1
     
-    return totalScores.map { sortedScores.firstIndex(of: $0)! + 1 }
+    for index in 0..<ordered.count {
+        let item = ordered[index]
+        let newAvg: Double = (Double(item[0]) + Double(item[1])) / 2
+        if newAvg != avg { 
+            avg = newAvg
+            rank = index + 1
+        }
+        
+        for i in 0..<score.count {
+           if item == score[i] {
+               result[i] = rank
+           }
+        }
+    }
+    
+    return result
 }
