506ffa7f1acd7ba9c5cbca9699365cd493446960
picnic <59194210+picnicodesm@users.noreply.github.com>
2025-10-15 17:33:22 +0900

    [level 2] Title: 조이스틱, Time: 0.81 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+func solution(_ name:String) -> Int {
+    let name = name.utf8.map{ min(Int($0) - 65, 90 - Int($0) + 1) }
+    let countUD = name.reduce(0, +)
+    var countRL = name.count - 1
+    
+    let idx = 0
+    for idxR in 0..<name.count {
+        var idxL = idxR + 1
+        while idxL < name.count && name[idxL] == 0 {
+            idxL += 1
+        }
+        let compareDistance = min(idxR - idx, name.count - idxL)
+        countRL = min(countRL, (idxR - idx) + (name.count - idxL) + compareDistance)
+    }
+    
+    return countUD + countRL
+}
