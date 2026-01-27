09429c679dfe1f5c75fbd196b240187c3bddc04a
picnic <59194210+picnicodesm@users.noreply.github.com>
2026-01-27 10:30:56 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 148 ms, Memory: 85584 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.swift
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.swift
@@ -1,14 +1,15 @@
 import Foundation
 
 let n = Int(readLine()!)!
-var points: [[Int]] = []
+var points: [(Int, Int)] = []
 
 for _ in 0..<n {
-    points.append(readLine()!.split(separator: " ").map { Int(String($0))! })
+    let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+    points.append((input[0], input[1]))
 }
 
 points = points.sorted {
-    return $0[0] == $1[0] ? $0[1] < $1[1] : $0[0] < $1[0]
+    return $0.0 == $1.0 ? $0.1 < $1.1 : $0.0 < $1.0
 }
 
-print(points.map { "\($0[0]) \($0[1])" }.joined(separator: "\n"))
+print(points.map { "\($0) \($1)" }.joined(separator: "\n"))
