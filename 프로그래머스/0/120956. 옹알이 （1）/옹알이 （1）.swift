be62b2347db8eb3254e2fc5c45469c1d81b66e37
picnic <59194210+picnicodesm@users.noreply.github.com>
2026-06-06 16:24:00 +0900

    [level 0] Title: 옹알이 (1), Time: 1.32 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.swift
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.swift
@@ -2,17 +2,15 @@ import Foundation
 
 func solution(_ babbling:[String]) -> Int {
     let words = ["aya", "ye", "woo", "ma"]
-    var isChange = false
     var babbling: [String] = babbling
     
-    babbling = babbling.map { 
+    babbling = babbling.filter {
         var s = $0
         for word in words {
-            s = s.replacingOccurrences(of: word, with: " ")
+            s = s.replacingOccurrences(of: word, with: "!")
         }
-        return s
+        return s.replacingOccurrences(of: "!", with: "").isEmpty
     }
     
-    
-    return babbling.filter { $0.replacingOccurrences(of: " ", with: "") == "" }.count
+    return babbling.count
 }
