6edf1d7987a4a1c5c8533d4fbbd6c17752b5c3ff
picnic <59194210+picnicodesm@users.noreply.github.com>
2026-06-26 07:52:56 +0900

    [level 1] Title: 약수의 합, Time: 0.74 ms, Memory: 20.9 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.swift
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.swift
@@ -1,14 +1,3 @@
-import Foundation
-
 func solution(_ n:Int) -> Int {
-    if n == 0 { return 0 }
-    var nums: [Int] = []
-    
-    for i in 1...n {
-        if n % i == 0 {
-            nums.append(i)
-        }
-    }
-    
-    return nums.reduce(0, +)
+    return n == 0 ? 0 : Array(1...n).filter { n % $0 == 0 }.reduce(0,+)
 }
